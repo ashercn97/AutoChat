82e116288757a2e25be1e5fc1ec1becc943e52c1
ashercn97 <131724380+ashercn97@users.noreply.github.com>
2024-02-27 15:23:42 -0500

    elts see if this works

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { AnthropicClient } from "./client/first.js";
 
-const client = new AnthropicClient(false);
+const client = new AnthropicClient(true);
 await client.setup();
 const res = await client.send("Hii i love claude!");
 console.log(res.output);
